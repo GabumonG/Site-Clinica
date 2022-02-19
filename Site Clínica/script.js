3d3f589c3a09f25537beb372da0dc2591a5328ae
Gabriela <gaby.gamergg@gmail.com>
2022-02-19 13:17:51 -0300

    Commit 19-02-2022
    Início da estrutura do site

---
+++ Site Clínica/script.js
@@ -0,0 +1,60 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+  //collapse/expand text
+  var acc = document.getElementsByClassName('accordion');
+  var i;
+  var len = acc.length;
+  for(i = 0; i < len; i++) {
+      acc[i].addEventListener('click', function() {
+          this.classList.toggle('active');
+          var panel = this.nextElementSibling;
+          if(panel.style.maxHeight){
+              panel.style.maxHeight = null;
+          } else {
+              panel.style.maxHeight = panel.scrollHeight + '300px';
+          }
+      })
+    }
