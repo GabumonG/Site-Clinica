0143c5da0e022f2d3e2e4fc8cacca12a0f375015
XiaoNaihe <GabrielSSK@outlook.com>
2022-04-08 11:35:52 -0300

    Modificações
    Modificação do site, rodapé e slider

--- Site Clínica/script.js
+++ Site Clínica/script.js
@@ -1,70 +1,93 @@
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
     }
-  
+
     animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+        this.navLinks.forEach((link, index) => {
+            link.style.animation ?
+                (link.style.animation = "") :
+                (link.style.animation = `navLinkFade 0.5s ease forwards ${
               index / 7 + 0.3
             }s`);
-      });
+        });
     }
-  
+
     handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
-  
+
     addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
-  
+
     init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
     }
-  }
-  
-  const mobileNavbar = new MobileNavbar(
+}
+
+const mobileNavbar = new MobileNavbar(
     ".mobile-menu",
     ".nav-list",
     ".nav-list li",
-  );
-  mobileNavbar.init();
+);
+mobileNavbar.init();
 
-  //collapse/expand text
-  const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+//collapse/expand text
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
-  accordionItemHeaders.forEach(accordionItemHeader => {
+accordionItemHeaders.forEach(accordionItemHeader => {
     accordionItemHeader.addEventListener("click", event => {
-      
-      // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-      
-      // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-      // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-      //   currentlyActiveAccordionItemHeader.classList.toggle("active");
-      //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-      // }
-  
-      accordionItemHeader.classList.toggle("active");
-      const accordionItemBody = accordionItemHeader.nextElementSibling;
-      if(accordionItemHeader.classList.contains("active")) {
-        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-      }
-      else {
-        accordionItemBody.style.maxHeight = 0;
-      }
-      
+
+        // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+
+        // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+        // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+        //   currentlyActiveAccordionItemHeader.classList.toggle("active");
+        //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        // }
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+
     });
-  });
+});
+
+/*Carrossel de convênios*/
+$(document).ready(function() {
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            setting: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            setting: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
