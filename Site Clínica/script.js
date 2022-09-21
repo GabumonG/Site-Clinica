04cabb83ba6596659980054c3578b7b1cf1ed864
Gabriela <gaby.gamergg@gmail.com>
2022-09-20 21:52:32 -0300

    Atualização 20-09-2022
    - Ajuste do espaçamento entre os convênios do slider do rodapé
    - Ajuste da responsividade do menu
    - Fixado o menu no topo mesmo quando descer a tela
    - Adicionado segundo mapa e botão de alternar entre os dois endereços

--- Site Clínica/script.js
+++ Site Clínica/script.js
@@ -91,3 +91,13 @@ $(document).ready(function() {
         }]
     });
 });
+
+
+/* CONTATOS*/
+function mostrar(id) {
+    if (document.getElementById(id).style.display == 'block') {
+      document.getElementById(id).style.display = 'none';
+    } else {
+      document.getElementById(id).style.display = 'block'; 
+    }        
+  }
